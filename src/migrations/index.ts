20f2b55f339d43f2bd8398d23d73bfc4eb715a7a
Michael Cervone <MCervone14@gmail.com>
2025-04-07 18:26:31 -0500

    remade migration with new db

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250407_230224_Init from './20250407_230224_Init';
+import * as migration_20250407_232447_Init from './20250407_232447_Init';
 
 export const migrations = [
   {
-    up: migration_20250407_230224_Init.up,
-    down: migration_20250407_230224_Init.down,
-    name: '20250407_230224_Init'
+    up: migration_20250407_232447_Init.up,
+    down: migration_20250407_232447_Init.down,
+    name: '20250407_232447_Init'
   },
 ];
