60def4116802b2daf4d7ed31f10206509ececa7f
Michael Cervone <MCervone14@gmail.com>
2025-04-07 19:51:32 -0500

    uncommented out some code in my user collection

--- src/payload/collections/Users/index.ts
+++ src/payload/collections/Users/index.ts
@@ -7,50 +7,50 @@ export const Users: CollectionConfig = {
   },
   fields: [],
   access: {
-    //   read: ({ req }) => {
-    //     if (req.user?.role === 'admin') {
-    //       return true
-    //     }
-    //
-    //     return {
-    //       id: { equals: req.user?.id },
-    //     }
-    //   },
-    //   create: () => true,
-    //   update: ({ req }) => req.user?.role === 'admin',
-    //   delete: ({ req }) => {
-    //     if (req.user?.role === 'admin') {
-    //       return true
-    //     }
-    //
-    //     return {
-    //       id: { equals: req.user?.id },
-    //     }
-    //   },
-    //   admin: ({ req }) => req.user?.role === 'admin',
-    // },
-    // hooks: {
-    //   beforeLogin: [
-    //     async ({ req, user }) => {
-    //       if (!user.email_verified && user.role === 'user') {
-    //         throw new Error('Please verify your email before logging in.')
-    //       }
-    //     },
-    //   ],
-    //   beforeChange: [
-    //     async ({ data, operation, req }) => {
-    //       if (operation === 'create') {
-    //         const existingUsers = await req.payload.find({
-    //           collection: 'users',
-    //           limit: 1,
-    //         })
-    //
-    //         if (existingUsers.totalDocs === 0) {
-    //           data.role = 'admin'
-    //         }
-    //       }
-    //       return data
-    //     },
-    //   ],
+    read: ({ req }) => {
+      if (req.user?.role === 'admin') {
+        return true
+      }
+
+      return {
+        id: { equals: req.user?.id },
+      }
+    },
+    create: () => true,
+    update: ({ req }) => req.user?.role === 'admin',
+    delete: ({ req }) => {
+      if (req.user?.role === 'admin') {
+        return true
+      }
+
+      return {
+        id: { equals: req.user?.id },
+      }
+    },
+    admin: ({ req }) => req.user?.role === 'admin',
+  },
+  hooks: {
+    beforeLogin: [
+      async ({ req, user }) => {
+        if (!user.email_verified && user.role === 'user') {
+          throw new Error('Please verify your email before logging in.')
+        }
+      },
+    ],
+    beforeChange: [
+      async ({ data, operation, req }) => {
+        if (operation === 'create') {
+          const existingUsers = await req.payload.find({
+            collection: 'users',
+            limit: 1,
+          })
+
+          if (existingUsers.totalDocs === 0) {
+            data.role = 'admin'
+          }
+        }
+        return data
+      },
+    ],
   },
 }
