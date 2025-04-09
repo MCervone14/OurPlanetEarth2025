f4421c348c5e034108140d0aaef752e038a6c68e
Michael Cervone <MCervone14@gmail.com>
2025-04-09 17:42:07 -0500

    Added remotePattern to next config file

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,19 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'unsplash.it'],
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: process.env.S3_ENDPOINT,
+          pathname: '/storage/v1/s3/our-planet-earth/*',
+        },
+        {
+          protocol: 'http',
+          hostname: localhost,
+        },
+      ],
+    },
   },
   productionBrowserSourceMaps: true,
 }
